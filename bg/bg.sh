b1e51f6b2bbe2041ad75347e622805062538b4f8
Yur1yPzdc <96118519+Yur1yPzdc@users.noreply.github.com>
2025-07-06 21:37:12 +0700

    Update bg.sh

--- bg/bg.sh
+++ bg/bg.sh
@@ -2,7 +2,7 @@
 
 values=(1 2 3 4 5 5 6 7 8 9)
 
-f="$HOME/bg/.last_img"
+f="$HOME/nixos-config/bg/.last_img"
 read -n1 l < "$f"
 
 v=$(( $l + 1 ))
@@ -12,9 +12,9 @@ if [ "$v" -eq "10" ]; then
 fi
 echo $v $l
 
-ask="$HOME/bg/$v.png"
+ask="$HOME/nixos-config/bg/$v.png"
 
-cat > $HOME/bg/.last_img << EOF1
+cat > $HOME/nixos-config/bg/.last_img << EOF1
 $v
 EOF1
 
