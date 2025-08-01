c9dbedca3f31b931ac5fa4883b980fd394d66dee
Yur1yPzdc <96118519+Yur1yPzdc@users.noreply.github.com>
2025-08-01 10:27:32 +0300

    remove unneeded option

--- home-manager/hyprland.nix
+++ home-manager/hyprland.nix
@@ -112,7 +112,6 @@
         animate_manual_resizes = true;
         animate_mouse_windowdragging = true;
         enable_swallow = true;
-        render_ahead_of_time = false;
         # force_default_wallpaper = -1;
         # disable_hyprland_logo = false;
         force_default_wallpaper = 1;
