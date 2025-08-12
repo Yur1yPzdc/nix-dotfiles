2c2fe8e0dc7d5e86a9e233eb8caf1de6e81372da
Yur1yPzdc <birinyurik@gmail.com>
2025-08-12 20:02:44 +0300

    Added discspace module

--- home-manager/waybar.nix
+++ home-manager/waybar.nix
@@ -7,15 +7,9 @@
         position = "top";
         y = 10;
 
-        modules-left = [ "hyprland/language" "keyboard-state" ];
+        modules-left = [ "hyprland/language" "keyboard-state" "custom/diskspace" ];
         modules-center = [ "clock" ];
         modules-right = [ "pulseaudio" "custom/battery" ];
-
-        "hyprland/workspaces" = {
-          disable-scroll = true;
-          min-length = 50;
-          tooltip = false;
-        };
 
         "hyprland/language" = {
           format-en = "Nya ฅ^•ﻌ•^ฅ ";
@@ -36,14 +30,21 @@
           tooltip = false;
         };
 
-        "clock" = {
-          timezone = "Asia/Novosibirsk";
-          locale = "ja_JP.utf8";
-          format = "{:L%a|%d日%B|%H:%M}";
+        "custom/diskspace" = {
+          format = "{}";
+          return-type = "text";
+          exec = "df -h | grep \"/$\" | awk '{print \"󰒋 \" $5}'";
+          interval = 60;
           tooltip = false;
         };
 
-        "custom/battery" = {
+        "clock" = {
+          timezone = "Asia/Novosibirsk";
+          locale = "ja_JP.utf8";
+          format = "{:L%a・%d日%B・%H:%M}";
+          tooltip = false;
+        };
+"custom/battery" = {
           format = "{}";
           return-type = "text";
           exec = "sh /home/yuri/nixos-config/scripts/for_waybar/battery.sh";
@@ -101,17 +102,29 @@ window#waybar.hidden {
 }
 
 #keyboard-state {
-    padding-right: 8px;
+    padding-right: 0px;
     padding-left: 0px;
-    border-radius: 0px 8px 8px 0px;
+    border-radius: 0px 0px 0px 0px;
     transition: none;
     color: #ffffff;
     background: #1a1623;
 }
 
+#custom-diskspace {
+    min-width: 40px;
+    padding-left: 0px;
+    padding-right: 8px;
+    border-radius: 0px 8px 8px 0px;
+    transition: none;
+    color: #ffffff;
+    background: #1a1623;
+    font-family: Ricty;
+    font-size: 18px;
+}
+
 #custom-battery {
     min-width: 60px;
-    padding-left: 8px;
+    padding-left: 0px;
     padding-right: 0px;
     border-radius: 0px 8px 8px 0px;
     transition: none;
@@ -122,7 +135,7 @@ window#waybar.hidden {
 }
 
 #pulseaudio {
-    min-width: 70px;
+    min-width: 60px;
     padding-right: 0px;
     padding-left: 8px;
     border-radius: 8px 0px 0px 8px;
