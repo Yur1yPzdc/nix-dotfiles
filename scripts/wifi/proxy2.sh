963a688d4907890a61ccedbd3a408ef3a6bf6648
Yur1yPzdc <birinyurik@gmail.com>
2025-08-15 18:27:28 +0700

    Extra ciadpi configs

---
+++ scripts/wifi/proxy2.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/home/yuri/nixos-config/scripts/wifi/ciadpi --daemon --split 1 --oob 1 --auto redirect --oob 1 --auto torst --fake -1 --tlsrec 1+s --auto ssl_error
---
+++ scripts/wifi/proxy3.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/home/yuri/nixos-config/scripts/wifi/ciadpi --daemon --split 2 --disorder 3 --fake -1 --ttl 5
