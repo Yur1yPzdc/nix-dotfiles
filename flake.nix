0cc878f430597ed9802e731e15124b65780581cd
Yur1yPzdc <birinyurik@gmail.com>
2025-10-11 19:19:47 +0300

    Flake stuff

--- flake.nix
+++ flake.nix
@@ -15,9 +15,9 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    waybar = {
-      url = "github:Alexays/Waybar/8516d457ad63880e359cb650581deb52d9fc3559";
-      inputs.nixpkgs.follows = "nixpkgs";
+    rust-analyzer-src = {
+      url = "github:rust-lang/rust-analyzer/2025-08-11";
+      flake = false;
     };
 
     rose-pine-hyprcursor = {
@@ -26,12 +26,23 @@
     };
   };
  
-  outputs = { self, nixpkgs, home-manager, nixvim, nur, waybar, ... } @inputs: {
+  outputs = { self, nixpkgs, home-manager, nixvim, rust-analyzer-src, nur, ... } @inputs: {
 
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = { inherit inputs; };
       modules = [ 
+        ({ pkgs, ... }: {
+        nixpkgs.overlays = [
+          (final: prev: {
+            rust-analyzer = prev.rust-analyzer.overrideAttrs (old: {
+              version = "2025-08-11";
+              src = rust-analyzer-src;
+              cargoHash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+            });
+          })
+        ];
+        })
         ./configuration.nix 
 	      nixvim.nixosModules.nixvim
       ];
@@ -39,18 +50,19 @@
   
     homeConfigurations.yuri = home-manager.lib.homeManagerConfiguration {
       pkgs = let
-        waybarOverlay = final: prev: {
-          waybar = waybar.packages."x86_64-linux".default;
-        };
+        # waybarOverlay = final: prev: {
+        #   waybar = waybar.packages."x86_64-linux".default;
+        # };
       in
         ( import nixpkgs {
           system = "x86_64-linux";
           overlays = [ 
             nur.overlays.default 
-            waybarOverlay 
           ];
       });
-      modules = [ ./home-manager/home.nix ];
+      modules = [
+        ./home-manager/home.nix 
+      ];
     };
   };
 }
