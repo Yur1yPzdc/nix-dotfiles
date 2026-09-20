05e6bb6a283e2e5edc380e15c11bd2ff579c7827
Yur1yPzdc <birinyurik@gmail.com>
2026-09-20 19:39:29 +0300

    update colors

--- nixvim/plugins/lualine2.nix
+++ nixvim/plugins/lualine2.nix
@@ -1,22 +1,22 @@
-let 
-  tabsss = 
+let
+  tabsss =
   ''
 function()
   local result = {}
-  
+
   for i = 1, vim.fn.tabpagenr("$") do
     local buflist = vim.fn.tabpagebuflist(i)
     local winnr = vim.fn.tabpagewinnr(i)
     local bufnr = buflist[winnr]
     local bufname = vim.fn.bufname(bufnr)
-    
+
     -- Get filename
     local filename = vim.fn.fnamemodify(bufname, ":t")
     filename = filename == "" and "[No Name]" or filename
     local extension = vim.fn.fnamemodify(filename, ":e")
     local icon = require'nvim-web-devicons'.get_icon(filename, extension)
-    icon = icon == "nil" and "" or icon
-    
+    icon = icon or ""
+
     -- Check modified
     local modified = ""
     for _, b in ipairs(buflist) do
@@ -25,17 +25,18 @@ function()
         break
       end
     end
-    
+
     -- Window count
     local window_count = #buflist
     local is_active = i == vim.fn.tabpagenr()
     local is_larger = i > vim.fn.tabpagenr()
     local is_smaller_special = i < vim.fn.tabpagenr() - 1
-    
+
     -- Build clickable tab
     local tab_content = string.format("%d:%s %s %s(%d) ", i, icon, filename, modified, window_count)
+    tab_content = tab_content:gsub("%%", "%%%%")
     local clickable_tab = string.format("%%%dT%s%%T", i, tab_content)
-    
+
     if is_active then
       table.insert(result, "%#CTabA#" .. clickable_tab .. "%#CTabI#%#TabLine#")
     else
@@ -46,26 +47,26 @@ function()
       end
     end
   end
-  
+
   -- Fill the rest
   table.insert(result, "%#TabLineFill#%T%=")
-  
+
   return table.concat(result, "")
-end 
+end
     '';
-  cond_50 = 
+  cond_50 =
   ''
     function()
     return vim.fn.winwidth(0) > 50
     end
   '';
-  cond_80 = 
+  cond_80 =
   ''
     function()
     return vim.fn.winwidth(0) > 80
     end
   '';
-  cond_100 = 
+  cond_100 =
   ''
     function()
     return vim.fn.winwidth(0) > 100
@@ -107,21 +108,24 @@ in
           left = "";
           right = "";
         };
-        theme.normal.c = {
-          bg = "#181616"; # Kanagawa Dragon bg
-          fg = "#7E9CD8"; # Kanagawa Dragon blue
-        };
-        theme.inactive.c = {
-          bg = "#181616";
-          fg = "#7E9CD8";
-        };
-        theme.normal.a = {
-          bg = "#7E9CD8";
-          fg = "#181616";
-        };
-        theme.inactive.a = {
-          bg = "#181616";
-          fg = "#7E9CD8";
+        theme = let
+          mode = accent: {
+            a = { fg = "#181616"; bg = accent; gui = "bold"; };
+            b = { fg = "#c5c9c5"; bg = "#282727"; };
+            c = { fg = "#c5c9c5"; bg = "#181616"; };
+          };
+        in {
+          normal = mode "#8ba4b0";
+          insert = mode "#87a987";
+          visual = mode "#a292a3";
+          replace = mode "#c4746e";
+          command = mode "#c4b28a";
+          terminal = mode "#8ea4a2";
+          inactive = {
+            a = { fg = "#a6a69c"; bg = "#181616"; };
+            b = { fg = "#a6a69c"; bg = "#181616"; };
+            c = { fg = "#a6a69c"; bg = "#181616"; };
+          };
         };
       };
 
@@ -135,90 +139,67 @@ in
         ];
 
         lualine_c = [
-          
+
           { # Vim mode
-            __unkeyed-1 = {
-              __raw = 
-              ''
-                'mode',
-                color = function()
-                  local mode_color = {
-                    n = '#C4746E',      -- NORMAL (autumnRed)
-                    i = '#8A9A7B',      -- INSERT (autumnGreen)
-                    v = '#7E9CD8',      -- VISUAL (crystalBlue)
-                    [''] = '#7E9CD8', -- VISUAL BLOCK
-                    V = '#7E9CD8',      -- VISUAL LINE 
-                    c = '#957FB8',      -- COMMAND (oniViolet)
-                    no = '#C4746E',     -- WAITING OPERATOR 
-                    s = '#E6C384',      -- SELECT (karasuYellow)
-                    S = '#E6C384',      -- SELECT LINE 
-                    [''] = '#E6C384', -- SELECT BLOCK 
-                    ic = '#E6C384',     --  
-                    R = '#957FB8',      -- REPLACE 
-                    Rv = '#957FB8',     -- VIRTUAL REPLACE 
-                    cv = '#C4746E',     --  
-                    ce = '#C4746E',     --  
-                    r = '#7FB4CA',      -- (springBlue)
-                    rm = '#7FB4CA',     --  
-                    ['r?'] = '#7FB4CA', --  
-                    ['!'] = '#C4746E',  -- EXTERNAL !command
-                    t = '#C4746E',      -- TERMINAL 
-                  }
-                  return { fg = mode_color[vim.fn.mode()], bg = '#181616', gui = 'bold' }
-                end
-              '';
-            };
+            __unkeyed-1 = "mode";
+            color.__raw = ''
+              function()
+                local colors = {
+                  n = '#8ba4b0', i = '#87a987', v = '#a292a3', V = '#a292a3',
+                  c = '#c4b28a', s = '#b6927b', S = '#b6927b',
+                  R = '#c4746e', r = '#b6927b', t = '#8ea4a2', ['!'] = '#c4746e',
+                  [string.char(22)] = '#a292a3', [string.char(19)] = '#b6927b',
+                }
+                return {
+                  fg = colors[vim.api.nvim_get_mode().mode:sub(1, 1)] or '#c5c9c5',
+                  bg = '#181616', gui = 'bold',
+                }
+              end
+            '';
             icon = "";
           }
-          
+
           { # Filesize
-            __unkeyed-2 = "filesize";
-            color.fg = "#C5C9C5"; # fujiWhite
+            __unkeyed-1 = "filesize";
+            color.fg = "#c5c9c5";
             color.bg = "#181616";
             color.gui = "bold";
             cond.__raw = cond_80;
           }
-          
+
           { # Location
-            __unkeyed-2 = "location";
-            color.fg = "#C5C9C5";
+            __unkeyed-1 = "location";
+            color.fg = "#c5c9c5";
             color.bg = "#181616";
             color.gui = "bold";
           }
-          
+
           { # Progress
-            __unkeyed-2 = "progress";
-            color.fg = "#C5C9C5";
+            __unkeyed-1 = "progress";
+            color.fg = "#c5c9c5";
             color.bg = "#181616";
             color.gui = "bold";
             cond.__raw = cond_80;
           }
         ];
         lualine_x = [
-          
+
           { # Working LSP
             __unkeyed-1 = {
-              __raw = 
+              __raw =
               ''
                 function()
-                  local msg = ""
-                  local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
-                  local clients = vim.lsp.get_clients()
-                  if next(clients) == nil then
-                    return msg
+                  local names = {}
+                  for _, client in ipairs(vim.lsp.get_clients({ bufnr = 0 })) do
+                    table.insert(names, client.name)
                   end
-                  for _, client in ipairs(clients) do
-                    local filetypes = client.config.filetypes
-                    if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-                      return client.name
-                    end
-                  end
-                  return msg
+                  table.sort(names)
+                  return table.concat(names, ", ")
                 end
               '';
             };
             color = {
-              fg = "#C5C9C5";
+              fg = "#c5c9c5";
               bg = "#181616";
               gui = "bold";
             };
@@ -227,13 +208,13 @@ in
           }
           # Offset for better positioning
           "%="
-          
+
           { # Git branchname
             __unkeyed-1 = "branch";
-            color.fg = "#957FB8"; # oniViolet
+            color.fg = "#8992a7";
             color.bg = "#181616";
             color.gui = "bold";
-            fmt.__raw = 
+            fmt.__raw =
             ''
                 function(str)
                   local max_length = 15
@@ -243,7 +224,7 @@ in
             cond.__raw = cond_50;
             icon = "";
           }
-          
+
           { # Difference
             __unkeyed-1 = "diff";
             symbols = {
@@ -252,19 +233,25 @@ in
               removed = " ";
             };
             diff_color = {
-              added.fg = "#8A9A7B";    # autumnGreen
-              modified.fg = "#E6C384"; # karasuYellow
-              removed.fg = "#C4746E";  # autumnRed
+              added.fg = "#87a987";
+              modified.fg = "#b6927b";
+              removed.fg = "#c4746e";
             };
             color.bg = "#181616";
             cond.__raw = cond_50;
           }
-          
+
           { # Warnings, errors from LSP
             __unkeyed-1 = "diagnostics";
             color.bg = "#181616";
             cond.__raw = cond_100;
             icon = "| ";
+            diagnostics_color = {
+              error.fg = "#c4746e";
+              warn.fg = "#c4b28a";
+              info.fg = "#8ba4b0";
+              hint.fg = "#8ea4a2";
+            };
           }
         ];
         lualine_y = [
@@ -280,14 +267,14 @@ in
           {
             # __unkeyed-1 = "tabs";
             __unkeyed-1.__raw = tabsss;
-            symbols.alternate_file = ""; 
+            symbols.alternate_file = "";
           }
         ];
         # Filename + its access options
-        lualine_x = [ 
+        lualine_x = [
           {
-            __unkeyed-2 = "filename";
-            color.fg = "#957FB8"; # oniViolet
+            __unkeyed-1 = "filename";
+            color.fg = "#a292a3";
             color.bg = "#181616";
             cond.__raw = cond_100;
             newfile_status = true;
